4b10e9f2f1302034ac09e4d2a134c9971ed75b7f
Dr-shannon <129344762+Dr-shannon@users.noreply.github.com>
2023-06-20 12:37:08 +0800

    Add files via upload

---
+++ 江开畅第八课作业/UI of jkc/CUItestDlg.cpp
@@ -0,0 +1,33 @@
+﻿// CUItestDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "UI of jkc.h"
+#include "afxdialogex.h"
+#include "CUItestDlg.h"
+
+
+// CUItestDlg 对话框
+
+IMPLEMENT_DYNAMIC(CUItestDlg, CDialogEx)
+
+CUItestDlg::CUItestDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_UI_OF_JKC_DIALOG, pParent)
+{
+}
+
+CUItestDlg::~CUItestDlg()
+{
+}
+
+void CUItestDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CUItestDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CUItestDlg 消息处理程序
---
+++ 江开畅第八课作业/UI of jkc/CUItestDlg.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// CUItestDlg 对话框
+
+class CUItestDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CUItestDlg)
+
+public:
+	CUItestDlg(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~CUItestDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_UI_OF_JKC_DIALOG };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+};
